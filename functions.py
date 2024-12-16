be1bbe184b0df04835f0072d5adff55a6cc16af3
Deounsamnang <Deounsamnang7@gmail.com>
2024-12-16 13:18:31 +0700

    Basic and Function

---
+++ functions.py
@@ -0,0 +1,22 @@
+# Function
+# print('A VALUE')
+
+# METHOD -> FUNCTION OF DATATYPE
+# print('A VALUE'.upper())
+# print('A VALUE'.title())
+# print('A VALUE'.lower())
+# print('A VALUE'.replace('A', '3'))
+
+# NEW FUNCTION
+# print(abs(-3), ' -> absolute value')
+# print(max(3, 5), ' -> max value')
+# print(min(3, 5), ' -> min value')
+# print(len('A VALUE'), ' -> length')
+
+# PYTHAGORAS THEOREM
+side_a = input('Enter side a: ')
+side_b = input('Enter side b: ')
+
+hypotenuse = ((int(side_a) ** 2) + (int(side_b) ** 2)) ** 0.5
+
+print('Hypotenuse: ', hypotenuse)
